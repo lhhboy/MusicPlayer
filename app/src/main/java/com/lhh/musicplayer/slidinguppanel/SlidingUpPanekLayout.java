3b3f670c6bcd58826d0b701fb7cbcd3328ce1fe5
lhhboy <1603614324@qq.com>
2019-11-02 12:11:01 +0800

    主界面xml

---
+++ app/src/main/java/com/lhh/musicplayer/slidinguppanel/SlidingUpPanekLayout.java
@@ -0,0 +1,16 @@
+package com.lhh.musicplayer.slidinguppanel;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+public class SlidingUpPanekLayout extends ViewGroup {
+    public SlidingUpPanekLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+
+}
